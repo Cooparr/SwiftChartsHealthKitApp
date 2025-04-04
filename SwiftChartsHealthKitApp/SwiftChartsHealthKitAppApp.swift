0053cfb11109b2cd831b7555ac0149ae62e179a6
Alex <a.cooparr@gmail.com>
2025-04-04 15:57:26 +0100

    Initial Commit

---
+++ SwiftChartsHealthKitApp/SwiftChartsHealthKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsHealthKitAppApp.swift
+//  SwiftChartsHealthKitApp
+//
+//  Created by Alexander Cooper on 04/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsHealthKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
