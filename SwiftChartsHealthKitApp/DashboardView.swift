d24ff51a6c256dd5cb8d4e89e5caa0052527b1a7
Alex <a.cooparr@gmail.com>
2025-04-15 10:08:17 +0100

    Added animations to the chart when user is scrolling / dragging along the steps chart.

--- SwiftChartsHealthKitApp/DashboardView.swift
+++ SwiftChartsHealthKitApp/DashboardView.swift
@@ -41,12 +41,21 @@ struct DashboardView: View {
     @AppStorage("hasSeenPermissionRequest") private var hasSeenPermissionRequest = false
     @State private var isShowingPermissionSheet = false
     @State private var selectedStat: HealthMetricContext = .steps
+    @State private var rawSelectedDate: Date? = nil
     
     var averageStepCount: Double {
         guard !hkManager.stepData.isEmpty else { return 0 }
         let totalSteps = hkManager.stepData.reduce(0) { $0 + $1.value }
         return totalSteps / Double(hkManager.stepData.count)
     }
+    
+    var selectedHealthMetric: HealthMetric? {
+        guard let rawSelectedDate else { return nil }
+        return hkManager.stepData.first {
+            Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date)
+        }
+    }
+    
     
     var body: some View {
         NavigationStack {
@@ -82,12 +91,25 @@ struct DashboardView: View {
                         .padding(.bottom, 12)
                         
                         Chart {
-                            ForEach(HealthMetric.Mockdata) { steps in
+                            if let selectedHealthMetric {
+                                RuleMark(x: .value("Selected Metrics", selectedHealthMetric.date, unit: .day))
+                                    .foregroundStyle(Color.secondary.opacity(0.3))
+                                    .offset(y: -10)
+                                    .annotation(
+                                        position: .top,
+                                        spacing: 0,
+                                        overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) {
+                                            annotationView
+                                        }
+                            }
+                            
+                            ForEach(hkManager.stepData) { steps in
                                 BarMark(
                                     x: .value("Date", steps.date, unit: .day),
                                     y: .value("Steps", steps.value)
                                 )
                                 .foregroundStyle(Color.pink.gradient)
+                                .opacity(rawSelectedDate == nil || steps.date == selectedHealthMetric?.date ? 1.0 : 0.3)
                             }
                             
                             RuleMark(y: .value("Average", averageStepCount))
@@ -95,6 +117,7 @@ struct DashboardView: View {
                                 .lineStyle(.init(lineWidth: 1, dash: [5]))
                         }
                         .frame(height: 150)
+                        .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
                         .chartXAxis {
                             AxisMarks {
                                 AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
@@ -148,9 +171,27 @@ struct DashboardView: View {
             } content: {
                 HealthKitPermissionView(hasSeen: $hasSeenPermissionRequest)
             }
-
+            
         }
         .tint(selectedStat.metricColor)
+    }
+    
+    var annotationView: some View {
+        VStack(alignment: .leading) {
+            Text(selectedHealthMetric?.date ?? .now, format: .dateTime.weekday(.abbreviated).month(.abbreviated).day())
+                .font(.footnote).bold()
+                .foregroundStyle(.secondary)
+            
+            Text(selectedHealthMetric?.value ?? 0, format: .number.precision(.fractionLength(0)))
+                .fontWeight(.heavy)
+                .foregroundStyle(.pink)
+        }
+        .padding(12)
+        .background {
+            RoundedRectangle(cornerRadius: 4)
+                .fill(Color(.secondarySystemBackground))
+                .shadow(color: .secondary.opacity(0.3), radius: 2, x: 2, y: 2)
+        }
     }
 }
 
